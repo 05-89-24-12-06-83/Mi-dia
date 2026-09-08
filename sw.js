22c3bd68b39384c1ca99c9d3669273cc2f9ff0b8
05-89-24-12-06-83 <chintoernesto@gmail.com>
2026-09-08 12:19:32 -0700

    Fix asset paths to use 'Icons' directory

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='mi-dia-v1.4-ui-final';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icons/icon.svg','icons/icon-192.png','icons/icon-512.png'];
+const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','Icons/icon.svg','Icons/icon-192.png','Icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
