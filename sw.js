ee2211581ee3db7b971001aa4db876755f159dc9
05-89-24-12-06-83 <chintoernesto@gmail.com>
2026-09-14 18:31:46 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE='mi-dia-v1.4q-visual-integrada';
+const ASSETS=['./','index.html','manifest.webmanifest','Icons/icon.svg','Icons/icon-192.png','Icons/icon-512.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),
+    self.clients.claim()
+  ]));
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(event.request).then(r=>r||caches.match('./')))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      if(event.request.method==='GET'&&response.ok){
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      }
+      return response;
+    }))
+  );
+});
+
+self.addEventListener('notificationclick',event=>{
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({type:'window',includeUncontrolled:true})
+      .then(clients=>clients[0]?clients[0].focus():self.clients.openWindow('./'))
+  );
+});
+
+self.addEventListener('message',event=>{
+  if(event.data&&event.data.type==='SKIP_WAITING') self.skipWaiting();
+});
